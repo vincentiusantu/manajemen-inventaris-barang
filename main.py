2cb5721a051a435dc0c04becda7e9763534f830d
RahmaKhairany <rahmakhairanyy@gmail.com>
2023-11-30 17:53:57 +0700

    tambah inventaris

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print("Hello World")
+import csv
 
-print("ayam")
+def inventaris():
+    try:
+        with open('inventaris.csv', 'r',newline=' ')as file:
+            baca = csv.DictReader(file)
+            inventaris = {row['Nama Barang']: {'Jumlah' : int(row['Jumlah']), 'Deskripsi' : row['Deskripsi']} for row in baca}
+    except FileNotFoundError:
+        inventaris ={}
+    return inventaris
+
+def simpan_item(inventaris):
+    with open('inventaris.csv', 'w', newline=' ')as file:
+        data = ['Nama','Jumlah','Deskripsi']
+        tulis = csv.DictWriter(file, data=data)
+        tulis.writeheader()
+        for nama, a in inventaris.item():
+            tulis.writerow(
+                {'Nama Barang': nama, 'Jumlah': a['Jumlah'], 'Deskripsi': a['Deskripsi']}
+            )
+
+def tambah_item(inventaris):
