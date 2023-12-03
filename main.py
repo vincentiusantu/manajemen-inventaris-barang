7793600b42ec3e4bf934e7341fb8adbeca7c7f93
vincentiusantu <vincentiusantu@gmail.com>
2023-12-03 19:36:22 +0700

    debugging some bugs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 
 def inventaris():
     try:
-        with open('inventaris.csv', 'r',newline=' ')as file:
+        with open('inventaris.csv', 'r',newline='')as file:
             baca = csv.DictReader(file)
             inventaris = {row['Nama Barang']: {'Jumlah' : int(row['Jumlah']), 'Deskripsi' : row['Deskripsi']} for row in baca}
     except FileNotFoundError:
@@ -10,11 +10,11 @@ def inventaris():
     return inventaris
 
 def simpan_item(inventaris):
-    with open('inventaris.csv', 'w', newline=' ')as file:
+    with open('inventaris.csv', 'w', newline='')as file:
         data = ['Nama Barang','Jumlah','Deskripsi']
-        tulis = csv.DictWriter(file, data=data)
+        tulis = csv.DictWriter(file, fieldnames=data)
         tulis.writeheader()
-        for nama, a in inventaris.item():
+        for nama, a in inventaris.items():
             tulis.writerow(
                 {'Nama Barang': nama, 'Jumlah': a['Jumlah'], 'Deskripsi': a['Deskripsi']}
             )
@@ -31,3 +31,16 @@ def tambah_item(inventaris):
     print(f"{jumlah}, {nama}(s) berhasil ditambahkan !")
 
 
+inventaris = inventaris()
+
+while True:
+    print("\nManajemen Inventaris Barang:")
+    print("1. Tambah Barang")
+    print("x. Exit")
+
+    choice = input("Pilih Menu :")
+    if choice == '1':
+        tambah_item(inventaris)
+        simpan_item(inventaris)
+    elif choice == 'x':
+        break
